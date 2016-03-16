1bf283126b3751c8c015f93ff8b903bba824dfd5
snf <snf@users.noreply.github.com>
2016-03-15 23:59:39 -0400

    fixing and prettifying tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,12 @@ pub use assembler::{Arch, Reloc, assemble, assemble2};
 
 #[test]
 fn x86_int3() {
-    //assert_eq!(assemble(Arch::X86, "int3").unwrap(), [0xcc]);
+    assert_eq!(assemble(Arch::X86, "int3").unwrap(), [0xcc]);
 }
 
 #[test]
 fn x86_jcc() {
-    let bytes = assemble(Arch::X86_64, "nop; jz here; nop; nop; here:").expect("couldn't compile the code");
+    let bytes = assemble(Arch::X86_64, "nop; jz here; nop; nop; here:").unwrap();
     assert_eq!(bytes, [0x90, 0x74, 0x02, 0x90, 0x90]);
 }
 
@@ -18,6 +18,11 @@ fn x86_jcc() {
 fn x86_jmp_rel() {
     let addr = 0x1000;
     let reloc = Reloc::new("here", 0x1003);
-    let bytes = assemble2(Arch::X86_64, "jmp here", addr, &[reloc]).expect("couldn't compile the code");
+    let bytes = assemble2(Arch::X86_64, "jmp here", addr, &[reloc]).unwrap();
     assert_eq!(bytes, [0xeb, 0x01]);
+}
+
+#[test]
+fn missing_label() {
+    assert_eq!(assemble(Arch::X86_64, "jz here"), None);
 }
